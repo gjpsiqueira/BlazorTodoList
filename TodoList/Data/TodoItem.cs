749b1c2242ad283b3d17850ba1edd29379b1ae4e
Gustavo Siqueira <gjpsiqueira@gmail.com>
2022-02-15 21:03:34 -0300

    Add todo

---
+++ TodoList/Data/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Data
+{
+    public class TodoItem
+    {
+
+        public Guid id { get; set; }
+        [Required]
+        public string description { get; set; }
+        public bool isComplete { get; set; }
+        public DateTime dateCreated { get; set; }
+    }
+}
---
+++ TodoList/Pages/Todo.razor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+using TodoList.Data;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json.Serialization;
+using Microsoft.JSInterop;
+
+namespace TodoList.Pages
+{
+    public partial class Todo : IAsyncDisposable
+    {
+
+        private EditContext editContext;
+        private TodoItem todoItem;
+        private List<TodoItem> todoItemList = new List<TodoItem>();
+        private IJSObjectReference jSObjectReference;
+
+        [Inject] private ProtectedLocalStorage BrowserStorage { get; set; }
+        [Inject] private IJSRuntime jSRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            todoItem = new();
+            editContext = new(todoItem);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                jSObjectReference = await jSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/todo.js");
+                var localStorageList = await jSObjectReference.InvokeAsync<string>("getLocalStorage");
+
+                if (localStorageList != null)
+                {
+                    List<TodoItem> a = JsonSerializer.Deserialize<List<TodoItem>>(localStorageList);
+                    todoItemList.AddRange(a);
+                    StateHasChanged();
+                }
+            }
+                
+        }
+
+        public void HandleSubmit()
+        {
+            if(editContext != null && editContext.Validate())
+            {
+                Console.WriteLine("Teste " + todoItem.description);
+                todoItemList.Add(new TodoItem()
+                    {
+                        description = todoItem.description,
+                        dateCreated = DateTime.Now,
+                        isComplete = false,
+                        id = Guid.NewGuid()
+                    }
+                ); ;
+
+                todoItem.description = "";
+            }
+            else
+            {
+                Console.WriteLine("Not Invalid!");
+            }
+            
+        }
+
+        public void HandleDelete(Guid guid)
+        {
+            todoItemList.Remove(todoItemList.Find(x => x.id == guid));
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+
+            await jSObjectReference.InvokeVoidAsync("setLocalStorage", todoItemList);
+            
+        }
+    }
+}
