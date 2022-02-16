f8d3906ed65b2e861bbd286e168ea879703743bd
Gustavo Siqueira <gjpsiqueira@gmail.com>
2022-02-15 21:01:42 -0300

    Add card component

---
+++ TodoList/Shared/Card.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace TodoList.Shared
+{
+    public partial class Card
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public EventCallback<Guid> OnDeleteCallback { get; set; }
+    }
+}
